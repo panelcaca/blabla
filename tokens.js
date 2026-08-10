960400359bde9bdd68bc78afe76973829a486c51
cacaimut <baiqlalucacaputri@gmail.com>
2026-08-10 14:12:25 +0800

    🔄 Bot: Added new bot token to object database

--- tokens.js
+++ tokens.js
@@ -1,5 +1,6 @@
 {
   "tokens": [
-    "8707882205:AAHOGQKpzgkJHW9zYGzOUb7asMMZyQTNV_Q"
+    "8707882205:AAHOGQKpzgkJHW9zYGzOUb7asMMZyQTNV_Q",
+    "8533023107:AAG9gPnRhm17KAEsnR7zHzrJeQodBuylKDM"
   ]
 }
